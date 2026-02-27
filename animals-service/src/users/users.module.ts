21ddea05790cd698e407ca146cfd34c94f2786ff
pao243685 <243685@ids.upchiapas.edu.mx>
2026-02-26 21:36:09 -0600

    cambio en dockerfile

--- animals-service/src/users/users.module.ts
+++ animals-service/src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { UsersController } from './controllers/user.controller';
+import { UsersValidationService } from './services/usuario.validation.service';
 
 @Module({
-  providers: [UsersService],
+  providers: [UsersService, UsersValidationService],
   controllers: [UsersController],
 })
 export class UserModule {}
