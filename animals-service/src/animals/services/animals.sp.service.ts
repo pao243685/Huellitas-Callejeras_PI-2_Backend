1875597acdc04d66a7b0606d90804c0902a19f6a
pao243685 <243685@ids.upchiapas.edu.mx>
2026-04-09 04:51:13 -0600

    dto con movimentos

--- animals-service/src/animals/services/animals.sp.service.ts
+++ animals-service/src/animals/services/animals.sp.service.ts
@@ -33,35 +33,40 @@ export class AnimalsSpService {
       ? new Date(dto.fecha_movimiento)
       : new Date();
 
-    await this.prisma.$queryRaw`
-      SELECT sp_registrar_animal_completo(
-        ${dto.nombre}::VARCHAR(100),
-        ${dto.especie}::VARCHAR(100),
-        ${dto.raza}::VARCHAR(100),
-        ${dto.edad}::INTEGER,
-        ${dto.peso}::DECIMAL(10,2),
-        ${dto.sexo}::TEXT,
-        ${tamanoDb}::TEXT,
-        ${dto.enfermedad_no_tratable}::BOOLEAN,
-        ${dto.discapacidad}::BOOLEAN,
-        ${dto.es_agresivo}::BOOLEAN,
-        ${dto.lugar}::TEXT,
-        ${dto.descripcion}::TEXT,
-        ${dto.refugio_id}::UUID,
-        ${dto.usuario_id}::UUID,
-        ${dto.url_imagen}::TEXT,
-        ${fechaMovimiento}::TIMESTAMP
-      )
-    `;
+    const result = await this.prisma.$queryRaw<
+      { sp_registrar_animal_completo: string }[]
+    >`
+    SELECT sp_registrar_animal_completo(
+      ${dto.nombre}::VARCHAR(100),
+      ${dto.especie}::VARCHAR(100),
+      ${dto.raza}::VARCHAR(100),
+      ${dto.edad}::INTEGER,
+      ${dto.peso}::DECIMAL(10,2),
+      ${dto.sexo}::TEXT,
+      ${tamanoDb}::TEXT,
+      ${dto.enfermedad_no_tratable}::BOOLEAN,
+      ${dto.discapacidad}::BOOLEAN,
+      ${dto.es_agresivo}::BOOLEAN,
+      ${dto.lugar}::TEXT,
+      ${dto.descripcion}::TEXT,
+      ${dto.refugio_id}::UUID,
+      ${dto.usuario_id}::UUID,
+      ${dto.estado}::TEXT,
+      ${dto.url_imagen}::TEXT,
+      ${fechaMovimiento}::TIMESTAMP
+    ) as id
+  `;
 
-    const animal = await this.prisma.animal.findFirst({
-      where: {
-        usuario_id: dto.usuario_id,
-        refugio_id: dto.refugio_id,
-      },
-      orderBy: {
-        createdAt: 'desc',
-      },
+    const animalId = result[0]?.sp_registrar_animal_completo;
+
+    if (!animalId) {
+      throw new BadRequestException(
+        'No se pudo obtener el ID del animal creado',
+      );
+    }
+
+    const animal = await this.prisma.animal.findUnique({
+      where: { id_animal: animalId },
       include: {
         imagenes: true,
         etiquetas: { include: { etiqueta: true } },
@@ -69,9 +74,7 @@ export class AnimalsSpService {
     });
 
     if (!animal) {
-      throw new BadRequestException(
-        'El animal no fue creado correctamente. Verifique los datos enviados.',
-      );
+      throw new BadRequestException('Animal no encontrado después de crear');
     }
 
     return animal;
